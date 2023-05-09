f4f7679aca35a1280582ae0e07860f56b466f5c8
Nicolettastr <nicolettastruggia@hotmail.com>
2023-05-09 15:50:17 +0000

    first home section, landing page image

---
+++ src/front/js/component/s1_Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const S1_Home = () => {
+  return <>Hola</>;
+};
+
+export default S1_Home;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,9 +1,14 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import S1_Home from "../component/s1_Home";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return <div className="text-center mt-5"></div>;
+  return (
+    <div className="text-center mt-5">
+      <S1_Home />
+    </div>
+  );
 };
